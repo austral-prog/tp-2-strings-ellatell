848b71227504a44030c12f035d382b189d7dc5c6
ellatell <etell@mail.austral.edu.ar>
2026-03-30 16:59:36 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -2,4 +2,12 @@ def check_vowels():
     """Lee un nombre y verifica si contiene cada una de las vocales (a, e, i, o, u),
     sin distinguir mayúsculas de minúsculas.
     """
-    pass
+    pass 
+    nombre = input ("Ingrese un nombre: ")
+    nombre = nombre.lower()
+    print ("El nombre contiene a: ", nombre.find("a"))
+    print("El nombre contiene e: ", nombre.find("e"))
+    print("El nombre contiene i: ", nombre.find("i"))
+    print("El nombre contiene o: ", nombre.find("o"))
+    print("El nombre contiene u: ", nombre.find("u"))
+
